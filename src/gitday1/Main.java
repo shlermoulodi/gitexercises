dcc3fe1b2ae8b8586f9eb6e735392d6019de48cb
Shler Moulodi <shlermoulodi@gmail.com>
2021-06-07 13:30:54 +0200

    added function that returns stirng to lower case

--- src/gitday1/Main.java
+++ src/gitday1/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String name = "Shler";
+        System.out.println(toLower(name));
+        System.out.println(toUpper(name));
+    }
+
+    private static String toUpper(String str) {
+        return str.toUpperCase();
+    }
+
+    private static String toLower(String s) {
+        return s.toLowerCase();
     }
 }
